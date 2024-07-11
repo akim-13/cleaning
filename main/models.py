ad25489d50722cbfa16aba00e2c4aee779c15cf3
Akim <akimkom@protonmail.com>
2024-07-11 14:38:45 +0300

    Started implementing models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+#from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+#class User(AbstractUser):
+    #pass
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=255)
+
+class Zone(models.Model):
+    zone_name = models.CharField(max_length=255)
+
+class Criterion(models.Model):
+    criterion_name = models.CharField(max_length=255)
+    mark_range_min = models.SmallIntegerField(default=0)
+    mark_range_max = models.SmallIntegerField(default=5)
+
+class Cell(models.Model):
+    mark = models.SmallIntegerField()
+    confirmation = models.BooleanField()
+    customer_comment = models.TextField()
+    customer_comment_time = models.DateTimeField(default=timezone.now)
+    customer_comment_photo = models.ImageField()
+    contractor_comment = models.TextField()
+    contractor_comment_time = models.DateTimeField(default=timezone.now)
+    contractor_comment_photo = models.ImageField()
