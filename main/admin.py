5a995e770fa44ff8014cec40ddc71bb4fa0ef571
Akim <akimkom@protonmail.com>
2024-07-14 14:26:33 +0300

    Registered models on the admin website

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cell, Location, Zone, Criterion, User
 
-# Register your models here.
+# FIXME: Most of these don't work on the admin website 
+# when trying to view or add e.g. a Cell or a Zone.
+admin.site.register(Cell)
+admin.site.register(Location)
+admin.site.register(Zone)
+admin.site.register(Criterion)
+admin.site.register(User)
