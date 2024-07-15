e49643012677a936d2585bf0c6c209ca48ad599b
Akim <akimkom@protonmail.com>
2024-07-15 17:02:06 +0300

    Changed on_delete in models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,3 @@
-#from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
@@ -11,16 +10,20 @@ class Location(models.Model):
 
 
 class User(models.Model):
+    # Relationships
+    location = models.ManyToManyField(Location)
+
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
-    # TODO: Not ForeignKey if many-to-many
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Zone(models.Model):
     # Relationships
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='zones_created', null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='zones_created', null=True, blank=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
 
     zone_name = models.CharField(max_length=255)
@@ -43,9 +46,10 @@ class Criterion(models.Model):
 
 class Cell(models.Model):
     # Relationships
+    # TODO: CASCADE or PROTECT?
     zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
-    criterion = models.ForeignKey(Criterion, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    criterion = models.ForeignKey(Criterion, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
 
     mark = models.SmallIntegerField()
