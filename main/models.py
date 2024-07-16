69951e1d750df9cdb4d2c06313fe812ffa86a619
Akim <akimkom@protonmail.com>
2024-07-16 14:24:44 +0300

    Minor changes to models, views, urls

--- main/models.py
+++ main/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 
+# TODO: Add editable=False to all fields that should be set automatically.
+# E.g. user = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, editable=False)
+# Currently, these are not set for debugging purposes.
 
 class Location(models.Model):
     location_name = models.CharField(max_length=255)
@@ -46,13 +49,13 @@ class Criterion(models.Model):
 
 class Cell(models.Model):
     # Relationships
-    # TODO: CASCADE or PROTECT?
+    # TODO: CASCADE or PROTECT for zone?
     zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
     criterion = models.ForeignKey(Criterion, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, editable=False)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
 
-    # TODO: Use mark_range_min and mark_range_max from Criterion (seems a bit complicated to implement).
+    # TODO: Use mark_range_min and mark_range_max from Criterion (seems har to implement atm).
     MARK_CHOICES = [(i, i) for i in range(0, 6)]
     mark = models.SmallIntegerField(choices=MARK_CHOICES)
     confirmation = models.BooleanField()
@@ -66,4 +69,4 @@ class Cell(models.Model):
     contractor_comment_photo = models.ImageField(upload_to='./media/contractor_photos/', null=True, blank=True)
 
     def __str__(self):
-        return (self.zone, self.criterion)
+        return (self.criterion, self.zone)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    return HttpResponse("This should be a registration/login page.")
