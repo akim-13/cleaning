db4709daac3a1df2d3fbb35921ef81f3170eb0f6
Akim <akimkom@protonmail.com>
2024-07-18 15:40:14 +0300

    Created a basic site layout

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('<location>/fill-out/', views.fill_out, name='fill_out'),
+    path('<location>/summary/', views.summary, name='summary'),
+    path('<location>/configure/', views.configure, name='configurator'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,34 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Cell, Location, Zone, User
 
-def index(request):
-    return HttpResponse("This should be a registration/login page.")
+
+def login(request):
+    return render(request, 'main/login.html')
+
+
+def register(request):
+    return render(request, 'main/register.html')
+
+
+def fill_out(request, location):
+    return render(request, 'main/fill_out.html')
+
+
+def summary(request, location):
+    # TODO: Get everything from the database.
+    # 1. Get all the zones for the location.
+    # 2. For each zone, get all its cells and calculate the average mark.
+    # 3. Calculate the total average mark for the location: sum(average marks) / count(zones).
+    zones = None
+    zones_average_marks = None 
+    total_average_mark = 4.8
+    context = { 
+        'zones': zones,
+        'zones_average_marks': zones_average_marks,
+        'total_average_mark': total_average_mark
+    }
+    return render(request, 'main/summary.html', context)
+
+
+def configure(request, location):
+    return render(request, 'main/configure.html')
